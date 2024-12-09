6b88682735fc0fbe7668d484ac3e699994857dde
saiabhinav75 <abhinavabhi7575@gmail.com>
2024-12-10 01:33:24 +0530

    audio-componentes-complete

--- app/(tabs)/audio/index.tsx
+++ app/(tabs)/audio/index.tsx
@@ -1,34 +1,48 @@
-import {StyleSheet,Text,View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Link, router } from 'expo-router';
+import { useRouter } from 'expo-router';
+import * as FileSystem from 'expo-file-system';
+import { useEffect, useState } from 'react';
+import AudioDisplay from '@/components/audioDisplay';
+
+
 
 export default function HomeScreen() {
+  const [files, setFiles] = useState<String[]>([]);
+  const router = useRouter();
+  const recordingsPath = `${FileSystem.documentDirectory}/recordings`
+  async function readFiles() {
+    const f = await FileSystem.readDirectoryAsync(recordingsPath);
+    setFiles(f)
+    console.log(f);
+  }
+  useEffect(() => {
+
+    readFiles();
+    return () => {
+
+    }
+  }, [])
+
   return (
     <SafeAreaView>
-      <View style={{width:100}}>
-      <Text>audio/index</Text>
-      <Link href={'/audio/recorder'}>record</Link>
-      <Link href={'/audio/player'}>player</Link>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingRight: '5%' }}>
+        <Text style={{ fontSize: 24 }}>Audio Library</Text>
+        <TouchableOpacity onPress={() => router.push({pathname:'/audio/recorder'})}>
+          <View style={{ borderRadius: 44 / 2, width: 44, height: 44, backgroundColor: '#ADD8E6', justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={{ fontSize: 28, textAlign: 'center' }}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View style={{ justifyContent: 'space-between', alignContent: 'space-between' }} >
+        {
+          files && files.map((name, index) => (
+            <View style={{ padding: '3%' }} key={index}>
+              <AudioDisplay name={String(name)} />
+            </View>
+          ))
+        }
       </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
--- app/(tabs)/audio/player.tsx
+++ app/(tabs)/audio/player.tsx
@@ -3,58 +3,98 @@ import React, { useState, useEffect } from 'react'
 import { Audio } from 'expo-av';
 import { Sound } from 'expo-av/build/Audio';
 import Entypo from '@expo/vector-icons/Entypo';
-import { useLocalSearchParams } from 'expo-router';
+import { useRouter, useLocalSearchParams } from 'expo-router';
 import * as FileSystem from 'expo-file-system';
 
 export default function player() {
   const [sound, setSound] = useState<Sound>();
-  const params:{"uri":string} = useLocalSearchParams();
-  console.log("params",params.uri)
-
-  const checkFileExists = async (uri:string) => {
-    const fileInfo = await FileSystem.getInfoAsync(uri);
-    console.log(fileInfo);
-    if (!fileInfo.exists) {
-      console.error('File does not exist at the provided URI:', uri);
-    } else {
-      console.log('File exists and is ready for playback:', uri);
-    }
-  };
-  checkFileExists(params.uri);
-
+  const [playbackStatus, setPlaybackStatus] = useState({
+    positionMillis: 0,
+    durationMillis: 1,
+  });
+  const [isPlaying, setIsPlaying] = useState(false);
+  const params: { "uri": string, "showSave": string } = useLocalSearchParams();
+  const router = useRouter();
+  console.log("params", params.uri)
   async function playSound() {
-    const Sound = new Audio.Sound();
-    if(!sound){
-
-      console.log('Loading Sound',params.uri);
-      const { sound } = await Audio.Sound.createAsync( {uri:params.uri});
+    if (!sound) {
+      const { sound } = await Audio.Sound.createAsync({ uri: params.uri });
       setSound(sound);
-
-      console.log('Playing Sound');
+      console.log(1)
+      sound.setOnPlaybackStatusUpdate((status) => {
+        if (status.isLoaded) {
+          setPlaybackStatus({
+            positionMillis: status.positionMillis,
+            durationMillis: status.durationMillis || 1, // Default to 1 if duration isn't available
+          });
+        }
+      });
+      setIsPlaying(true);
+      // console.log('Playing Sound');
       await sound.playAsync();
     }
-    else{
-      console.log('Pausing Sound')
+    else if (playbackStatus.positionMillis === playbackStatus.durationMillis) {
+      console.log(2)
+      await sound.replayAsync()
+      setIsPlaying(true);
+    }
+    else if (!isPlaying && playbackStatus.positionMillis < playbackStatus.durationMillis) {
+      // console.log("Playing Sound");
+      console.log(3)
+      setIsPlaying(true)
+      await sound.playAsync()
+    }
+    else if (isPlaying) {
+      // console.log("Pausing Sound")
+      console.log(4)
+      setIsPlaying(false)
       await sound.pauseAsync()
     }
   }
   useEffect(() => {
     return sound
       ? () => {
-          console.log('Unloading Sound');
-          sound.unloadAsync();
-        }
+        console.log('Unloading Sound');
+        sound.unloadAsync();
+      }
       : undefined;
   }, [sound]);
+
+  // play on end of duration
+  // useEffect(()=>{
+  //   return playbackStatus.positionMillis === playbackStatus.durationMillis && sound
+  //  ? () => {
+  //   console.log("Unloading Sound");
+  //   sound.unloadAsync();
+  //   setSound(undefined);
+  //  }: undefined
+
+  // },[playbackStatus])
+  async function handleSave() {
+    console.log("Clicked Save")
+    const rootDir = `${FileSystem.documentDirectory}recordings/`
+    const folderInfo = await FileSystem.getInfoAsync(rootDir);
+    if (!folderInfo.exists) {
+      await FileSystem.makeDirectoryAsync(rootDir, { intermediates: true });
+    }
+    const destination = `${rootDir}${params.uri.split("/").pop()}`;
+    await FileSystem.moveAsync({ from: params.uri, to: destination })
+
+    console.log("File Saved to", destination)
+    router.replace('/audio')
+  }
   return (
-    <View style={{justifyContent:'center', alignItems:'center',paddingTop:50}}>
+    <View style={{ justifyContent: 'center', alignItems: 'center', paddingTop: 50 }}>
       <Image source={require('../../../assets/images/playerImage.png')} resizeMethod='resize' />
-      <Text style={{fontWeight:'bold', fontSize:28}} >Audio Recorded</Text>
-      <TouchableOpacity onPress={playSound}>
-        <View style={{backgroundColor:'blue', padding:5, alignContent:'center', borderCurve:'circular'}} >
+      <Text style={{ fontWeight: 'bold', fontSize: 28 }} >Audio Recorded</Text>
+      <TouchableOpacity onPress={playSound} style={{ padding: 5 }}>
+        <View style={{ backgroundColor: '#ADD8E6', padding: 10, alignContent: 'center', borderCurve: 'circular', }} >
           <Entypo name="controller-play" size={24} color="white" />
         </View>
       </TouchableOpacity>
+      {!(params.showSave === 'false') ? <TouchableOpacity style={{ padding: 20, alignContent: 'center', backgroundColor: '#ADD8E6', minWidth: '90%', justifyContent: 'center', alignItems: 'center' }} onPress={handleSave}>
+        <Text style={{ fontSize: 20 }}>Save Audio</Text>
+      </TouchableOpacity> : <></>}
     </View>
   )
 }
